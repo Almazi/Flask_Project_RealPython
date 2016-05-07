cf6ac18ccb0913531b5a6cc80430a848e7d961dc
Almazi <navilalmazi@gmail.com>
2016-05-07 23:17:08 +0600

    Added login option on /login url on app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #import the F;ask and render_template class from flask module
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 #Create flask object and assign to app
 app = Flask(__name__)
@@ -16,33 +16,18 @@ def hello():
 def welcome():
     return render_template('welcome.html')
 
-@app.route('/search/<search_queries>')
-def search(search_queries):
-    return search_queries
+@app.route('/login', methods = ['GET', 'POST']) #HTTP method which is by default is a GET request when we dont mention it
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'Almazi' or request.form['password'] != 'Almazi':
+            error = "Wrong credentials. Please type again."
+        else:
+            return redirect(url_for('welcome')) #after giving correct credentials it will 302 redirect to welcome page
 
-@app.route("/integer/<int:value>")
-def int(value):
-    value = value + value**5
-    print(value) #This will print on terminal / ide
-    return "Integering" #This will show on browser
-@app.route("/float/<float:value>")
-def float(value):
-    value = value / 1.0
-    print(value)
-    return format(value) #Showing the value on browser after formatting
+    return render_template('login.html', error = error)
 
-@app.route("/check/<path:value>")
-def check(value):
-    print(value)
-    return "UTF Path"
 
-#Will add the response along with status code at the end, which is optional but good for RESTful API
-@app.route("/response/<name>")
-def responses(name):
-    if name.lower() == "almazi":
-        return "The name is zi, {}".format(name), 200
-    else:
-        return "Not found", 404
 
 #Running app
 if __name__ == '__main__':
