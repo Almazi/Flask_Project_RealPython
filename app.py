f7d40d23f8cbb374bb5c786dbd7d94a1c56fc3b8
Almazi <navilalmazi@gmail.com>
2016-05-09 00:44:06 +0600

    Added user authentication, login, flash message, logout and login only page redirection, index.html

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
 #import the F;ask and render_template class from flask module
-from flask import Flask, render_template, redirect, url_for, request
-
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from functools import wraps #for login decorators
 #Create flask object and assign to app
 app = Flask(__name__)
-
+app.secret_key = "My Layday" #adding secret key to make a session encryption key. Session relys on cookie. This key helps to access actual data on server side
 app.config["DEBUG"] = True #This will do same as: app.run(debug = 'True')
+
+#login decorators
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash("You need to login first!")
+            return redirect(url_for('login'))
+    return wrap
+
 
 #Use decorators to link functions to url
 @app.route('/')
-@app.route('/hello')
-def hello():
-    return "Hello, World!"
+@app.route('/home')
+@login_required
+def home():
+    #return "Hello, World!"
+    return render_template('index.html')
 
 @app.route('/welcome')
 def welcome():
@@ -23,11 +37,18 @@ def login():
         if request.form['username'] != 'Almazi' or request.form['password'] != 'Almazi':
             error = "Wrong credentials. Please type again."
         else:
-            return redirect(url_for('welcome')) #after giving correct credentials it will 302 redirect to welcome page
+            flash('You are just logged in')
+            session['logged_in'] = True
+            return redirect(url_for('home')) #after giving correct credentials it will 302 redirect to welcome page
 
     return render_template('login.html', error = error)
 
-
+@app.route('/logout')
+@login_required
+def logout():
+    flash('You are just logged out!')
+    session.pop('logged_in', None) #Pop the logged_in key and delete it. If someone goes to /logout route then we pop out the logged_in key and replace with empty None dictionary
+    return redirect(url_for('welcome')) #this home is functions name, will redirect to home function after going to logout url
 
 #Running app
 if __name__ == '__main__':
