a382b991d3615824842579e59b26f5b0a6fee6bf
majhi <navilalmazi@gmail.com>
2016-05-12 01:49:40 +0600

    Tweaking list comprehen on home functions

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def home():
         posts.append(post_dictionary) #it will add the dictionary key and value to list
         #print(posts) #it will show the list
         #posts.append(dict(title = row[0], description = row[1]))
-
+    posts.append(post_dictionary)
     #posts = [dict(title = row[0], description = row[1]) for row in qur.fetchall()] #list comprehenson of same for loop above: after fetching the data from posts it will save it in a dictionary "posts"
     g.db.close() #closing the database
     return render_template('index.html', posts = posts) #passing the posts list to template
